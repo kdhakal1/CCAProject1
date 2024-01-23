41a7b84545d7d279da8147b7c70c4d3af07351c2
kdhakal1 <114708647+kdhakal1@users.noreply.github.com>
2024-01-22 20:17:16 -0500

    Update serve.py

---
+++ serve.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import socket
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def stress_cpu():
+    subprocess.Popen(["python3", "stress_cpu.py"])
+    return jsonify({"message": "CPU stress initialized"})
+
+@app.route("/", methods=["GET"])
+def get_private_ip():
+    private_ip = socket.gethostbyname(socket.gethostname())
+    return private_ip
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
